3bfa5b36fc29ff301edef47fbcf603b55a4e3231
6666 <1327185450@qq.com>
2024-06-11 10:34:04 +0800

    portal

---
+++ src/app/portal/layout.tsx
@@ -0,0 +1,13 @@
+import { PortalBanner } from '@/components/portal/banner';
+import React from 'react';
+
+const Portallayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex flex-col md:h-screen ">
+      <PortalBanner />
+      <div className="container flex justify-center flex-1 h-0 mt-12">{children}</div>
+    </div>
+  );
+};
+
+export default Portallayout;
