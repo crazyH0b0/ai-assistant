899d666838305317c9769ef0bdec5ee603ee2eb2
6666 <1327185450@qq.com>
2024-06-11 22:01:44 +0800

    预约查看

---
+++ src/app/portal/[domainId]/appointment/[customerId]/page.tsx
@@ -0,0 +1,25 @@
+import { onDomainCustomerResponses, onGetAllDomainBookings } from '@/actions/appointment';
+import PortalForm from '@/components/forms/portal/portal-form';
+import React from 'react';
+
+type Props = { params: { domainId: string; customerId: string } };
+
+const CustomerSignUpForm = async ({ params }: Props) => {
+  const questions = await onDomainCustomerResponses(params.customerId);
+  const bookings = await onGetAllDomainBookings(params.domainId);
+
+  if (!questions) return null;
+
+  return (
+    <PortalForm
+      bookings={bookings}
+      email={questions.email!}
+      domainId={params.domainId}
+      customerId={params.customerId}
+      questions={questions.questions}
+      type="Appointment"
+    />
+  );
+};
+
+export default CustomerSignUpForm;
