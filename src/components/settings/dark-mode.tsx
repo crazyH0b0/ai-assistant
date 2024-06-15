e046bc50cda340a065f0cf4fda0f688f34b13a31
6666 <1327185450@qq.com>
2024-06-15 16:21:41 +0800

    暗黑模式切换样式

--- src/components/settings/dark-mode.tsx
+++ src/components/settings/dark-mode.tsx
@@ -1,56 +1,54 @@
-"use client"
-import { useThemeMode } from '@/hooks/settings/use-settings'
-import React from 'react'
-import Section from '../section-label'
-import { cn } from '@/lib/utils'
-import { SystemMode } from '../themes/systemmode'
-import { DarkMode } from '../themes/darkmode'
-import { LightMode } from '../themes/lightmode'
+'use client';
+import { useThemeMode } from '@/hooks/settings/use-settings';
+import React from 'react';
+import Section from '../section-label';
+import { cn } from '@/lib/utils';
+import { SystemMode } from '../themes/systemmode';
+import { DarkMode } from '../themes/darkmode';
+import { LightMode } from '../themes/lightmode';
 
-interface Props {
-  
-}
+interface Props {}
 
 const DarModeToggle = (props: Props) => {
-  const {theme, setTheme} = useThemeMode()
+  const { theme, setTheme } = useThemeMode();
   return (
-    <div className='grid grid-cols-1 lg:grid-cols-5 gap-10'>
-      <div className='lg:col-span-1 '>
-        <Section
-        label='界面主题'
-        message='选择或者定制你的主题'
-        />
+    <div className="grid grid-cols-1 lg:grid-cols-5 gap-10">
+      <div className="lg:col-span-1 ">
+        <Section label="界面主题" message="选择或者定制你的主题" />
       </div>
-        <div className='lg:col-span-4 flex lg:flex-row flex-col items-start gap-5'>
-          <div className={cn('rounded-2xl overflow-hidden cursor-pointer border-4 border-transparent',
-            theme === 'system' && 'border-orange'
-          )}
-          onClick={()=> setTheme('system')}
-          >
-            <SystemMode />
-          </div>
-
-          <div className={cn('rounded-2xl overflow-hidden cursor-pointer border-4 border-transparent',
+      <div className="lg:col-span-4 flex lg:flex-row flex-col items-start gap-5">
+        <div
+          className={cn(
+            'rounded-2xl overflow-hidden cursor-pointer border-4 border-transparent',
             theme === 'dark' && 'border-orange'
           )}
-          onClick={()=> setTheme('dark')}
-          >
-            <DarkMode />
-          </div>
+          onClick={() => setTheme('dark')}
+        >
+          <DarkMode />
+        </div>
 
-          <div className={cn('rounded-2xl overflow-hidden cursor-pointer border-4 border-transparent',
+        <div
+          className={cn(
+            'rounded-2xl overflow-hidden cursor-pointer border-4 border-transparent',
             theme === 'light' && 'border-orange'
           )}
-          onClick={()=> setTheme('light')}
-          >
-            <LightMode />
-          </div>
-      
+          onClick={() => setTheme('light')}
+        >
+          <LightMode />
+        </div>
 
+        <div
+          className={cn(
+            'rounded-2xl overflow-hidden cursor-pointer border-4 border-transparent',
+            theme === 'system' && 'border-orange'
+          )}
+          onClick={() => setTheme('system')}
+        >
+          <SystemMode />
+        </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default DarModeToggle
+export default DarModeToggle;
