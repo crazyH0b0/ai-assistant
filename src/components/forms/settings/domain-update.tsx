a10bbd4bf3e0f73b36c4ac2165fbfd95705158f8
6666 <1327185450@qq.com>
2024-06-17 23:52:10 +0800

    修改样式

--- src/components/forms/settings/domain-update.tsx
+++ src/components/forms/settings/domain-update.tsx
@@ -1,26 +1,28 @@
-import React from 'react'
+import React from 'react';
 
-import { FieldErrors, FieldValues, UseFormRegister } from 'react-hook-form'
-import FormGenerator from '../form-generator'
+import { FieldErrors, FieldValues, UseFormRegister } from 'react-hook-form';
+import FormGenerator from '../form-generator';
 
 type DomainUpdateProps = {
-  name: string
-  register: UseFormRegister<FieldValues>
-  errors: FieldErrors<FieldValues>
-}
+  name: string;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors<FieldValues>;
+};
 
 export const DomainUpdate = ({ name, register, errors }: DomainUpdateProps) => {
   return (
     <div className="flex gap-2 pt-5 items-end w-[400px]">
-      <FormGenerator
-        label="域名名称"
-        register={register}
-        name="domain"
-        errors={errors}
-        type="text"
-        inputType="input"
-        placeholder={name}
-      />
+      <div className="pl-2 ">
+        <FormGenerator
+          label="域名名称"
+          register={register}
+          name="domain"
+          errors={errors}
+          type="text"
+          inputType="input"
+          placeholder={name}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
