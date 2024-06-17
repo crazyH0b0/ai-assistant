52efaaa5a901d8d914a181f4958b1fbec8e1807e
6666 <1327185450@qq.com>
2024-06-18 00:17:12 +0800

    修改重定向

--- src/app/(dashboard)/settings/[domain]/page.tsx
+++ src/app/(dashboard)/settings/[domain]/page.tsx
@@ -1,35 +1,32 @@
-import { onGetCurrentDomain } from '@/actions/settings'
-import BotTraningForm from '@/components/forms/settings/bot-traning'
-import SettingsForm from '@/components/forms/settings/form'
-import InfoBar from '@/components/infobar'
-import { redirect } from 'next/navigation'
-import React from 'react'
+import { onGetCurrentDomain } from '@/actions/settings';
+import BotTraningForm from '@/components/forms/settings/bot-traning';
+import SettingsForm from '@/components/forms/settings/form';
+import InfoBar from '@/components/infobar';
+import { redirect } from 'next/navigation';
+import React from 'react';
 
 interface Props {
-  params: {domain: string}
+  params: { domain: string };
 }
 
-const DomainSettingPage =async ({params}: Props) => {  
-  const domain = await onGetCurrentDomain(params.domain) 
-  if(!domain ) return redirect("/dashboard")
-  
+const DomainSettingPage = async ({ params }: Props) => {
+  const domain = await onGetCurrentDomain(params.domain);
+  if (!domain) return redirect('/settings');
+
   return (
     <>
-      <InfoBar /> 
-      <div className='overflow-y-auto w-full chat-window flex-1 h-0'>
+      <InfoBar />
+      <div className="overflow-y-auto w-full chat-window flex-1 h-0">
         <SettingsForm
-        plan={domain.subscription?.plan}
-        chatBot={domain.domains[0].chatBot}
-        id={domain.domains[0].id}
-        name={domain.domains[0].name}
+          plan={domain.subscription?.plan}
+          chatBot={domain.domains[0].chatBot}
+          id={domain.domains[0].id}
+          name={domain.domains[0].name}
         />
-        <BotTraningForm
-         id={domain.domains[0].id}
-        />
-
+        <BotTraningForm id={domain.domains[0].id} />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default DomainSettingPage
+export default DomainSettingPage;
