7b2627ca75b83bb592c026d28a1c06c21a707822
6666 <1327185450@qq.com>
2024-06-17 23:52:29 +0800

    代码片段说明

--- src/components/settings/code-snippet.tsx
+++ src/components/settings/code-snippet.tsx
@@ -1,16 +1,15 @@
-"use client"
+'use client';
 
-import React from 'react'
-import { useToast } from '../ui/use-toast'
-import Section from '../section-label'
-import { Copy } from 'lucide-react'
+import React from 'react';
+import { useToast } from '../ui/use-toast';
+import Section from '../section-label';
+import { Copy } from 'lucide-react';
 
 interface Props {
-  id: string
+  id: string;
 }
-
-const CodeSnippet = ({id}: Props) => {
-  const { toast } = useToast()
+const CodeSnippet = ({ id }: Props) => {
+  const { toast } = useToast();
   let snippet = `
     const iframe = document.createElement("iframe");
     
@@ -26,10 +25,12 @@ const CodeSnippet = ({id}: Props) => {
             bottom: 50px;
             right: 50px;
             border: none;
-        }
-    ')
-    
-    iframe.src = "http://localhost:3000/chatbot"
+            }
+            ')
+            
+          
+    iframe.src = "https://ai-assistant-pts3.vercel.app/chatbot"
+
     iframe.classList.add('chat-frame')
     document.body.appendChild(iframe)
     
@@ -38,32 +39,32 @@ const CodeSnippet = ({id}: Props) => {
         let dimensions = JSON.parse(e.data)
         iframe.width = dimensions.width
         iframe.height = dimensions.height
-        iframe.contentWindow.postMessage("${id}", "http://localhost:3000/")
-    })`
+        iframe.contentWindow.postMessage("${id}", "https://ai-assistant-pts3.vercel.app/")
+    })`;
+  // process.env.PRO_URL
   return (
-    <div className='mt-10 flex flex-col gap-5 items-start'>
-      <Section label='复制代码' message='把该段代码复制到网站的 header 标签中'/>
+    <div className="mt-10 flex flex-col gap-5 items-start">
+      <Section
+        label="复制代码"
+        message="把该段代码复制到第三方网站的 header 标签中 (注意：部分编辑器报错需要手动添加“``”（反引号）)"
+      />
       <div className="bg-cream px-10 rounded-lg inline-block relative">
         <Copy
           className="absolute top-5 right-5 text-gray-400 cursor-pointer"
           onClick={() => {
-            navigator.clipboard.writeText(snippet)
+            navigator.clipboard.writeText(snippet);
             toast({
               title: '已复制到粘贴板',
               description: '你现在可以将该代码粘贴到你的网站中',
-            })
+            });
           }}
         />
         <pre>
           <code className="text-gray-500">{snippet}</code>
         </pre>
       </div>
-
-
-     
-      
     </div>
-  )
-}
+  );
+};
 
-export default CodeSnippet
+export default CodeSnippet;
